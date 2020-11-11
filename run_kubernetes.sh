7feb2736569ffc572e320bc276f369a8f3f909e7
Rafael Gonzalez <rcgonzsv@aol.com>
2020-11-11 08:09:11 -0600

    v0.2.0

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,23 +1,17 @@
 #!/usr/bin/env bash
 
-# This tags and uploads an image to Docker Hub
-
-# Step 1:
-# This is your Docker ID/path
-# dockerpath=<>
 dockerpath="vonbolan/mlapi"
 
-# Step 2
-# Run the Docker Hub container with kubernetes
-kubectl run mlmicroserviceapi\
-    --generator=run-pod/v1\
-    --image=$dockerpath\
-    --port=80 --labels app=mlmicroserviceapi
+# Run in Docker Hub container with kubernetes
+#kubectl run \
+    #--generator=run-pod/v1 mlmicroservicesapi\
+    #--image=$dockerpath\
+    #--port=80 --labels app=mlmicroservicesapi
+kubectl run --generator=run-pod/v1 mlmicroservicesapi --image=$dockerpath
+#kubectl run --generator=run-pod/v1 mlmicroservicesapi --image=vonbolan/mlapi
 
-# Step 3:
 # List kubernetes pods
 kubectl get pods
 
-# Step 4:
-# Forward the container port to a host
-kubectl port-forward mlmicroserviceapi 8000:80
+# Forward the container port to host
+kubectl port-forward mlmicroservicesapi 8000:80
