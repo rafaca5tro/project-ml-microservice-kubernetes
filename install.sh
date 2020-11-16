a2c59e7c5e8d05c6e863ae8a6a5d81a8d9586f1d
Rafael Gonzalez <rcgonzsv@aol.com>
2020-11-15 21:58:35 -0600

    test 3

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ python3 -m venv venv
 . venv/bin/activate
 make install
 # Install hadolint
-wget -O /bin/hadolint https://github.com/hadolint/hadolint/releases/download/v1.16.3/hadolint-Linux-x86_64 &&\
-chmod +x /bin/hadolint
+sudo wget -O /bin/hadolint https://github.com/hadolint/hadolint/releases/download/v1.16.3/hadolint-Linux-x86_64 &&\
+sudo chmod +x /bin/hadolint
